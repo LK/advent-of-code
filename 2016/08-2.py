a61b25e1811faa22d1b98f8903e222800eb29982
Lenny Khazan <lenny.khazan@gmail.com>
2016-12-10 10:53:34 -0500

    Day 8

---
+++ 2016/08-2.py
@@ -0,0 +1,40 @@
+import re
+from copy import deepcopy
+
+screen = [[0] * 50 for i in range(6)]
+
+def show():
+	print '\n'.join(''.join('X' if x else ' ' for x in row) for row in screen)
+
+def rect(width, height):
+	for w in range(width):
+		for h in range(height):
+			screen[h][w] = 1
+
+def rotate_col(col, by):
+	copy = deepcopy(screen)
+	for i in range(6):
+		screen[(i+by)%6][col] = copy[i][col]
+
+def rotate_row(row, by):
+	copy = deepcopy(screen)
+	for i in range(50):
+		screen[row][(i+by)%50] = copy[row][i]
+
+line = raw_input()
+while line != '':
+	m = re.match(r'rect (\d*?)x(\d*)', line)
+	if m != None:
+		rect(int(m.group(1)), int(m.group(2)))
+
+	m = re.match(r'rotate column x=(.*?) by (.*)', line)
+	if m != None:
+		rotate_col(int(m.group(1)), int(m.group(2)))
+
+	m = re.match(r'rotate row y=(.*?) by (.*)', line)
+	if m != None:
+		rotate_row(int(m.group(1)), int(m.group(2)))
+
+	line = raw_input()
+
+show()
