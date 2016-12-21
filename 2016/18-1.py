4d6a17326cf070465117f80880753ef204d1d241
Lenny Khazan <lenny.khazan@gmail.com>
2016-12-20 19:06:56 -0500

    Day 18

---
+++ 2016/18-1.py
@@ -0,0 +1,20 @@
+rows = [raw_input()]
+
+for i in range(39):
+	row = ''
+	prev = '.' + rows[-1] + '.'
+	for j in range(len(rows[0])):
+		if prev[j:j+3] == '^^.':
+			row += '^'
+		elif prev[j:j+3] == '.^^':
+			row += '^'
+		elif prev[j:j+3] == '^..':
+			row += '^'
+		elif prev[j:j+3] == '..^':
+			row += '^'
+		else:
+			row += '.'
+
+	rows.append(row)
+		
+print sum([r.count('.') for r in rows])
